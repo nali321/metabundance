8dc451fee82430a21333717856e1d019a233aea4
Numair <numairali321@gmail.com>
2023-04-01 18:22:17 -0500

    fixed

--- metabundance.py
+++ metabundance.py
@@ -76,31 +76,31 @@ config_path = methods.config(outdir, d)
 #call snakemake
 os.system(f"snakemake --cores {sc} --directory {outdir} --snakefile {snake_dir} all --configfile {config_path}")
 
-# #collect all rgi, genomad, integron output files
-# os.system(f"bash {home_dir}/scripts/gather_annotations.sh {rp_total-1} {outdir}")
+#collect all rgi, genomad, integron output files
+os.system(f"bash {home_dir}/scripts/gather_annotations.sh {rp_total-1} {outdir}")
 
-# #create the FASTA and .faa files
-# uid_tracker, protein_tracker, fasta_path, faa_path, head = methods.fasta(f"{outdir}/all_rgi", f"{outdir}/fasta")
+#create the FASTA and .faa files
+uid_tracker, protein_tracker, fasta_path, faa_path, head = methods.fasta(f"{outdir}/all_rgi", f"{outdir}/fasta")
 
-# #create config file for abundance run
-# d = {"output": outdir, "reads": reads_path, "sample": sample_ids,
-#     "conda_path": conda_profile, "envs_path": envs_path,
-#     "illuminaclip": illuminaclip, "all_args": fasta_path,
-#     "muscle": f"{dep}/muscle3.8.31_i86linux64", 
-#     "usearch": f"{dep}/usearch11.0.667_i86linux32", 
-#     "CARD_markers": f"{dep}/ShortBRED_CARD_2017_markers.faa", "rule_all": "abundance"}
+#create config file for abundance run
+d = {"output": outdir, "reads": reads_path, "sample": sample_ids,
+    "conda_path": conda_profile, "envs_path": envs_path,
+    "illuminaclip": illuminaclip, "all_args": fasta_path,
+    "muscle": f"{dep}/muscle3.8.31_i86linux64", 
+    "usearch": f"{dep}/usearch11.0.667_i86linux32", 
+    "CARD_markers": f"{dep}/ShortBRED_CARD_2017_markers.faa", "rule_all": "abundance"}
 
-# #create 2nd config file
-# config_path = methods.config(outdir, d)
+#create 2nd config file
+config_path = methods.config(outdir, d)
 
-# #second call of snakemake
-# os.system(f"snakemake --cores {sc} --directory {outdir} --snakefile {snake_dir} all --configfile {config_path}")
+#second call of snakemake
+os.system(f"snakemake --cores {sc} --directory {outdir} --snakefile {snake_dir} all --configfile {config_path}")
 
-# #gather all kallisto and shortbred output files
-# os.system(f"bash {home_dir}/scripts/gather_abundance.sh {rp_total-1} {outdir}")
+#gather all kallisto and shortbred output files
+os.system(f"bash {home_dir}/scripts/gather_abundance.sh {rp_total-1} {outdir}")
 
-# #gather kallisto and shortbred counts
-# first_col = methods.counts(uid_tracker, f"{outdir}/all_kallisto", f"{outdir}/all_shortbred", f"{outdir}/matrices")
+#gather kallisto and shortbred counts
+first_col = methods.counts(uid_tracker, f"{outdir}/all_kallisto", f"{outdir}/all_shortbred", f"{outdir}/matrices")
 
-# #create observation table
-# methods.observations(uid_tracker, head, first_col, metadata, f"{outdir}/matrices")
+#create observation table
+methods.observations(uid_tracker, head, first_col, metadata, f"{outdir}/matrices")
