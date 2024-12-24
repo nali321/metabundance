7444ef184dc6f98f9636a71eb7cbeda22b273737
Numair <numairali321@gmail.com>
2024-12-24 15:51:35 -0600

    config file fix

--- metabundance.py
+++ metabundance.py
@@ -145,7 +145,7 @@ else:
 d = {"output": outdir, "reads": reads_path, "sample": sample_ids,
     "conda_path": conda_profile, "envs_path": envs_path,
     "illuminaclip": illuminaclip, "fasta": f"{fasta_path}/annotations.FASTA",
-    "protein": faa_path, "snp_path": "N/A", "snp_db": "N/A",
+    "protein": faa_path, "cat_path": "N/A", "cat_db": "N/A",
     "muscle": f"{dep}/muscle3.8.31_i86linux64", "usearch": f"{dep}/usearch11.0.667_i86linux32", 
     "CARD_markers": f"{dep}/ShortBRED_CARD_2017_markers.faa", "rule_all": "abundance"}
 
